d0979b5290e340ae8785a701d340c5d5b4277bc2
yehia mahmoud <m_yehia_computer@yahoo.com>
2022-02-17 10:12:22 +0200

    add modal to get the data and select it

--- src/app/demographicinfo/demographicinfo.component.ts
+++ src/app/demographicinfo/demographicinfo.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { empty } from 'rxjs';
 import { Router } from '@angular/router';
 import { DemographicInfo, DemographicInfoDetails } from '../interfaces/demographicInfo.interface';
 import { DemoGraphicService } from '../services/demographic.service';
+import { Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-demographicinfo',
@@ -11,7 +12,6 @@ import { DemoGraphicService } from '../services/demographic.service';
   styleUrls: ['./demographicinfo.component.css']
 })
 export class DemographicinfoComponent {
-  http: HttpClient;
   service: DemoGraphicService;
   router: Router;
 
@@ -20,21 +20,21 @@ export class DemographicinfoComponent {
   public newDemoGraphicDetail: DemographicInfoDetails ={ choicesAr: '', choicesEn: '', weightValue: 0, demTypeDtlId: 0, demTypeId: 0 };
 
 
-  constructor(http: HttpClient,  service: DemoGraphicService, router: Router) {
-    this.http = http;
+  constructor(service: DemoGraphicService, router: Router) {
     this.service = service;
     this.router = router;
+    this.demoGraphicGetAll();
   }
 
 
   demoGraphicGetAll() {
     this.service.demoGraphicGetAll().subscribe(result => {
       this.demoGraphicList = result;
+      console.log(result);
     }, error => console.error(error));
   }
 
-  demoGraphicGetById(id: string) {
-
+  demoGraphicGetById(id: any) {
     if (id) {
       this.service.demoGraphicGetById(Number(id)).subscribe(result => {
         if (result !== null) {
@@ -103,5 +103,14 @@ export class DemographicinfoComponent {
 
   //  return true;
   //}
+
+// <----- modal ----->
+  displayStyle = "none";  
+  openPopup(): void {
+    this.displayStyle = "block";
+  }
+  closePopup(): void {
+    this.displayStyle = "none";
+  }
 
 }
