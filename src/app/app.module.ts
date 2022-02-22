2728c7e9b95ec770511386b757b6e7718d811706
amr-CS <amr.mahmoud.mohamed15@gmail.com>
2022-02-22 16:53:17 +0200

    Add side bar and nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,44 @@ import { AppComponent } from './app.component';
 import { DemographicinfoComponent } from './demographicinfo/demographicinfo.component';
 import { DemographicfilterPipe } from './pipes/demographicfilter.pipe';
 import { DatePipe } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarModule } from 'ng-sidebar'
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule} from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+
+
 
 @NgModule({
-  declarations: [
+  declarations: [				
     AppComponent,
     DemographicinfoComponent,
-    DemographicfilterPipe
-  ],
+    DemographicfilterPipe,
+      HeaderComponent,
+      SidebarComponent,
+      HomeComponent,
+      DashboardComponent
+   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SidebarModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatMenuModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [DatePipe],
   bootstrap: [AppComponent]
